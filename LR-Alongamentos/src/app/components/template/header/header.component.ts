2f102e34569faa37a820bcc6c6a73e2059f57b02
Jonathas Rosa <jonathasrosa85@gmail.com>
2023-05-29 10:24:17 -0300

    Update LR-Alongamentos

--- LR-Alongamentos/src/app/components/template/header/header.component.ts
+++ LR-Alongamentos/src/app/components/template/header/header.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +6,5 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  isHandset$: Observable<boolean>;
-
-  constructor(private breakpointObserver: BreakpointObserver) {
-    this.isHandset$ = this.breakpointObserver.observe(Breakpoints.Handset)
-      .pipe(
-        map(result => result.matches)
-      );
-  }
 
 }
